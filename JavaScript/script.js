061f28233ec1c243d8eaae6b6db3549ac3b9ffb2
abuhasan-Ohovro <160999306+abuhasan-Ohovro@users.noreply.github.com>
2024-02-26 00:45:34 +0600

    first code

---
+++ JavaScript/script.js
@@ -0,0 +1,20 @@
+//A simple program with function to add two number
+/*
+function add(){
+    let a=20;
+    let b=30;
+    let x=a+b;
+    console.log(x);
+}
+add() */
+
+//This example uses multi-line comments to prevent the execution of addition code and perform subtraction operations.
+/*
+function subtraction(){
+    let a=18;
+    let b=20;
+    let x=b-a;
+    console.log(b-a);
+}
+subtraction() 
+*/
